2efb7eabbbb11147ceb31123f9fbef3e71927905
[Matthieu40] <[mlegagneur@smu.edu]>
2020-09-07 23:54:48 -0500

    Made some progress on prob generator.

--- Project1/HelloWorldMidi/src/ProbabilityGenerator.java
+++ Project1/HelloWorldMidi/src/ProbabilityGenerator.java
@@ -12,12 +12,44 @@ public class ProbabilityGenerator<T> {
 	
 	void train(ArrayList<T> newTokens) {
 		//code training
-		
+		for(int i = 0; i < newTokens.size() -1; i++){
+			
+
+			T index = alphabet.get(i);
+			
+			if(index == null) {
+				
+				T w = newTokens.get(i);
+				alphabet.add(w);
+				
+				alphabet_counts.add(0);
+			}
+			
+			alphabet_counts.get((int) index);
+			
+		}
 	}
+	
 	
 	T generate() {
 		T newToken = null;
 		//do something here (generate 1 token)
+		float rIndex = (float) Math.random();
+		
+		for(int i=0; i< alphabet.size(); i++) {
+			
+		}
+		
 		return newToken;
+	}
+	
+	ArrayList<T> generate(int length)
+	{
+		ArrayList<T> newSequence = new ArrayList<T>();
+		for (int i = 0;i<length; i++) {
+			newSequence.add(generate());
+		}
+		
+		return newSequence;
 	}
 }
