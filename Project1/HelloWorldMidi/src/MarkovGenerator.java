0784642c4332d6e439323fee169a226c9f2a50a4
[Matthieu40] <[mlegagneur@smu.edu]>
2020-09-21 22:48:40 -0500

    project 2 part 1 sofar

--- Project1/HelloWorldMidi/src/MarkovGenerator.java
+++ Project1/HelloWorldMidi/src/MarkovGenerator.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class MarkovGenerator<T> extends ProbabilityGenerator<T>{
-
+	
 	
 	MarkovGenerator(){
 		super();
@@ -16,6 +16,43 @@ public class MarkovGenerator<T> extends ProbabilityGenerator<T>{
 	
 	
 	void train(ArrayList<T> newTokens) {
+		//to declare
+		ArrayList<ArrayList<Integer>> transitionTable;
+
+		//to create the ArrayList
+		transitionTable = new ArrayList();
+
+		//to add a row
+		ArrayList<Integer> myRow = new ArrayList();
+		myRow.add(8); //any number
+
+		//to get the 0th (i.e., first) ArrayList or row from our ArrayList of Arraylists:
+
+		ArrayList<Integer> row = transitionTable.get(0);
+
+		//then to get the 0th element from the row (assuming it is both not empty & has been instantiated)
+		Integer myElement = row.get(0);
+
+		//System.out.println(row.get(0)); //will print 8.
+		
+		int lastIndex = -1;
+		
+		for(int i = 0; i < alphabet.size(); i++ ) {
+			
+			int tokenIndex = alphabet.size();
+			transitionTable.add(myRow);
+			
+			for(int x = 0; x < transitionTable.size(); x++) {
+				
+				for(int j = 0; j < transitionTable.get(i).size(); j++) {
+					
+					transitionTable.add(0);
+					
+				}
+				
+			}
+			
+		}
 		
 	}
 	
