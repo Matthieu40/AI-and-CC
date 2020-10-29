52317edf6767876d5f0237a608f34cbb7c18530c
[Matthieu40] <[mlegagneur@smu.edu]>
2020-10-29 04:04:46 -0500

    Have a lot of syntax errors to work out

--- Project5/src/Node.java
+++ Project5/src/Node.java
@@ -2,29 +2,36 @@
  * c2020-2021 Matthieu Legagneur
  * 10/25/20
  */
+import java.io.PrintStream;
 import java.util.*;
-public class Node <Node>extends tree{
+public class Node <Node, T>extends tree{
 	
 	ArrayList<Node> children = new ArrayList<>();
-	ArrayList<Node> tokenSequence = new ArrayList<>();//check to see if tokensequence is meant to be array list and check for data type
+	ArrayList<T> tokenSequence = new ArrayList<T>();//check to see if tokensequence is meant to be array list and check for data type
 	
-	//constructor (ask about how to correctly format it
-	<T> Node(ArrayList<T> curSequence) {
+	//constructor 
+	Node(ArrayList<T> curSequence) {
 		// TODO Auto-generated constructor stub
+		tokenSequence = curSequence;
 	}
 
 	boolean amIASuffix(Node node) {//ask for clarification with this function
 		//do something here:
+		if(node.getTokenSequence().sublist(0,node.getTokenSequence().size()-1).equals(tokenSequence.subList(0, tokenSequence.size()-1))) {
+			return true;
+		}else {
 		return false;
+		}
 	}
 	
 	boolean addNode(Node node) {
 		boolean found = false;
+		//node.setTokenSequence(tokenSequence);
 		if(tokenSequence == node) {
 			found = true;
 		}else if(amIASuffix(node)||(tokenSequence.size()==0)) {
 			
-			if(found = false && ((ArrayList<Node>) node).size()/*check syntax for node's tokenSequence*/ < tokenSequence.size()) {
+			if(found = false && node.getTokenSequence() < tokenSequence.size()) {//is node supposed to be a new obj? why is this wrong?
 					children.add(node);
 					found = true;
 			}
@@ -39,7 +46,7 @@ public class Node <Node>extends tree{
 			//figure out how to print each node in children(syntax)
 			//temp
 			for(int i = 0; i< children.size(); i++) {
-				//node.print(1);
+				((PrintStream) children.get(i)).print(1);
 			}
 	}
 	
@@ -49,8 +56,16 @@ public class Node <Node>extends tree{
 			System.out.println(tokenSequence.get(i));
 		}
 		for(int j = 0; j < children.size();j++) {
-			//node.print(numSpacesBefore+1); // Figure out proper syntax
+			((PrintStream) children.get(j)).print(numSpacesBefore+1); // Figure out proper syntax
 		}
+	}
+	
+//	void setTokenSequence(ArrayList<T> tokenSequence) {
+//		this.tokenSequence = new ArrayList<T>();
+//	}
+	
+	ArrayList<T> getTokenSequence() {
+		return tokenSequence;
 	}
 	
 }
--- Project5/src/main.java
+++ Project5/src/main.java
@@ -4,5 +4,26 @@
  */
 import java.util.*;
 public class main {
-
+	
+	
+	public static void main(String[] args) {
+		Integer[] myList = {3, 3, 3, 5, 5, 7, 7, 7, 2, 3, 5};
+		ArrayList<Integer> testList = new ArrayList(Arrays.asList(myList));
+		
+		char[] list1 = {'a','b','r','a','c','a','d','a','b','r','a'};
+		ArrayList<String> List1 = new ArrayList(Arrays.asList(list1));
+		
+		char[] list2 = {'a','c','a','d','a','a','c','b','d','a'};
+		ArrayList<String> List2 = new ArrayList(Arrays.asList(list2));
+		
+		char[] list3 = {'a','b','c','c','c','d','a','a','d','c','d','a','a','b','c','a','d','a','d'};
+		ArrayList<String> List3 = new ArrayList(Arrays.asList(list3));
+		
+		Node node = new Node(testList);
+		Node node2 = new Node(List2);
+		Node node3 = new Node(List3);
+		
+		node.train(List1);
+	}
+	
 }
